8b1416bfde8e3d2306715430edee33b77af3d314
Edel Marcelino <edelm8@gmail.com>
2019-09-09 20:14:12 -0700

    added display for Sup's Sales figs

--- bamazonSupervisor.js
+++ bamazonSupervisor.js
@@ -5,6 +5,9 @@ var mysql = require("mysql");
 var inquirer = require("inquirer");
 var clear = require("clear");
 var colors = require("colors");
+const {
+    table
+} = require('table');
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -15,29 +18,27 @@ var connection = mysql.createConnection({
 });
 
 connection.connect(err => {
-    if(err) throw err;
+    if (err) throw err;
     console.log(`connected as ID ${connection.threadId}`);
     clear();
     main();
 });
 
-function main(){
-    inquirer.prompt([
-        {
-            name: "supervisorMenu",
-            type: "list",
-            message: "Select Option: ",
-            choices: ["View Product Sales by Department", "Create New Department", "Quit"]
-        }
-    ]).then(resp => {
-        switch(resp.managerMenu){
+function main() {
+    inquirer.prompt([{
+        name: "supervisorMenu",
+        type: "list",
+        message: "Select Option: ",
+        choices: ["View Product Sales by Department", "Create New Department", "Quit"]
+    }]).then(resp => {
+        switch (resp.supervisorMenu) {
             case "View Product Sales by Department":
-                clear();
-                
+                //clear();
+                salesByDept();
                 break;
             case "Create New Department":
                 clear();
-                
+                createDept();
                 break;
             case "Quit":
                 process.exit(1);
@@ -47,4 +48,56 @@ function main(){
                 main();
         }
     });
+}
+
+function salesByDept() {
+    var config;
+    var data = [["Dept. ID", "Dept. Name", "Total Sales", "Overhead", "Total_Profit"]];
+
+    connection.query("SELECT departments.department_id, departments.department_name, SUM(products.product_sales) AS totalProduct_sales, departments.over_head_costs FROM products RIGHT JOIN departments ON products.department_name = departments.department_name GROUP BY department_name", (err, resp) => {
+        if (err) throw err;
+
+        resp.forEach(element => {
+            var temp = [];
+            temp.push(element.department_id);
+            temp.push(element.department_name);
+            temp.push(element.totalProduct_sales);
+            temp.push(element.over_head_costs);
+            temp.push(element.totalProduct_sales - element.over_head_costs);
+            data.push(temp);
+        });
+        clear();
+        console.log(colors.bold.green("\nSales Data by Department"));
+        console.log(table(data))
+        main();
+    });
+    
+}
+
+function createDept() {
+    inquirer.prompt([
+        {
+            name: "name",
+            type: "input",
+            message: "What is the name of the NEW department? "
+        },
+        {
+            name: "cost",
+            type: "input",
+            message: "What are the overhead cost for new Dept.?"
+        }
+    ]).then(answers => {
+        connection.query(
+            `INSERT INTO departments SET ?`, {
+                department_name: answers.name,
+                over_head_costs: answers.cost
+            },
+            (err, res) => {
+                if (err) throw err;
+                console.log("Create " + res.affectedRows + "new Dept named: " + answers.name);
+                main();
+            }
+        );
+    });
+
 }
--- bamazon_seeds.sql
+++ bamazon_seeds.sql
@@ -1,6 +1,6 @@
 INSERT INTO products (product_name, department_name, price, stock_quantity, product_sales) VALUES
 ("Nvidia RTX 2080", "Computers", 999.99, 10, 0.00),
-("LG 80in OLED TV", "Electonics", 2999.99, 5, 0.00),
+("LG 80in OLED TV", "Electronics", 2999.99, 5, 0.00),
 ("Nespresso Vertio", "Home Goods", 119.99, 20, 0.00),
 ("15in Macbook Pro", "Computers", 1999.99, 15, 0.00),
 ("MTG Booster Box", "Toys & Games", 99.99, 100, 0.00),
@@ -11,4 +11,14 @@ INSERT INTO products (product_name, department_name, price, stock_quantity, prod
 ("Mystery Box", "Misc", 100.00, 1, 0.00);
 
 SELECT * FROM products;
+
+INSERT INTO departments (department_name, over_head_costs) VALUES
+("Computers", 1000.00),
+("Electronics", 2500.00),
+("Home Goods", 500.00),
+("Toys & Games", 200.00),
+("Pet Supplies", 150.00),
+("Food & Grocery", 3500.00),
+("Misc", 5.00);
+
 SELECT * FROM departments;
